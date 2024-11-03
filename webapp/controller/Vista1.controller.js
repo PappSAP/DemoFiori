bb9db98cb765cd594f69654ea55c8548afb00132
PappSAP <pponce@outlook.com>
2024-11-03 15:47:29 +0000

    Back02

--- webapp/controller/Vista1.controller.js
+++ webapp/controller/Vista1.controller.js
@@ -1,12 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
 ],
-function (Controller) {
+/**
+ * 
+ * @param {*} Controller 
+ * @param {*} UIComponent 
+ * @returns 
+ */
+function (Controller,UIComponent) {
     "use strict";
 
     return Controller.extend("demofiori.controller.Vista1", {
         onInit: function () {
 
+        },
+        onPressVista1: function() {
+            let oRouter = UIComponent.getRouterFor(this);
+            oRouter.navTo("RouteVista11");
+
+
         }
+
     });
 });
